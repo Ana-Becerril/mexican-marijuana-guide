30ad175a24660006a0ece45f4b26dac7a51aa013
Ana- Becerril <ana.becbel@gmail.com>
2021-03-16 18:21:58 -0600

    added an icon whose have an animation for search

--- home.js
+++ home.js
@@ -1,3 +1,6 @@
+import search from    './src/search.svg'
+
+
 function home (){
 
     const homeContainer=document.createElement("div");
@@ -5,18 +8,28 @@ function home (){
 
     const homeBox=document.createElement("div");
     homeBox.classList.add("home-box")
+
     const homeTittle=document.createElement("div");
     homeTittle.innerHTML="Marijuana Guide"
     homeTittle.classList.add("home-tittle");
 
-    const iconSearch=document.createElement("div")
-    iconSearch
+    const iconSearchContainer=document.createElement("div")
+    iconSearchContainer.classList.add("icon-container");
+    iconSearchContainer.classList.add("bounce-in-left")
+    const iconSearch=document.createElement("img");
+    iconSearch.src = search;    
+    iconSearch.classList.add("icon-search");
+
+    const input=document.createElement("input");
+    input.classList.add("input");
+    input.type=Text
 
     const homeDescription=document.createElement("div");
     homeDescription.innerHTML="Flavors and Effects"
     homeDescription.classList.add("home-description");
    
-    homeBox.append(homeTittle, homeDescription);
+    iconSearchContainer.appendChild(iconSearch);
+    homeBox.append(homeTittle, iconSearchContainer, homeDescription);
     homeContainer.appendChild(homeBox);
     return homeContainer;
     
